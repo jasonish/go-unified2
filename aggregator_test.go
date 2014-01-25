0fc869ed06b6802c339e00b9a73758e2774e1b1e
Jason Ish <ish@unx.ca>
2014-01-24 18:42:29 -0600

    Aggregator example.

--- aggregator_test.go
+++ aggregator_test.go
@@ -109,3 +109,48 @@ func TestCompleteEvent(t *testing.T) {
 		t.Fatalf("expected 1 record in aggregator")
 	}
 }
+
+// EventAggregator example.
+func ExampleEventAggregator() {
+
+	// Create the aggregator.
+	aggregator := NewEventAggregator()
+
+	// Open a file.  Note that the aggregator is meant to span the
+	// input of multiple files, as the records that make up a single
+	// event may span multiple files.
+	file, err := os.Open("merged.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Submit records to the aggregator, it will return non-nil when a
+	// complete event has been seen.
+	for {
+		recordHolder, err := ReadRecord(file)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			// Unexpected error.
+			log.Fatal(err)
+		}
+
+		event := aggregator.Add(recordHolder)
+		if event != nil {
+			log.Printf("We have an event consisting of %d records.\n",
+				len(event))
+		}
+	}
+
+	// Since we hit EOF we may not have triggered the last event to be
+	// flushed, so check.
+	event := aggregator.Flush()
+	if event != nil {
+		log.Printf("Final event flushed\n")
+	} else {
+		// Unlikely to happen.
+		log.Printf("No remaining events.")
+	}
+
+}
